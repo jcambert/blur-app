29dc8b8fb4f65372047314725c23c6b1c90d18ad
jcambert <jc.ambert@free.fr>
2018-01-08 22:47:18 +0100

    hook update

--- api/hooks/compositeHook/index.js
+++ api/hooks/compositeHook/index.js
@@ -9,91 +9,116 @@
 'use strict'
 
 module.exports = function eventsHook(app) {
-    
-  return {
 
-    initialize(done) {
-    
-    var models = sails.models;
-    _.each(models,model=>{
-        _.each(model.attributes,(field,key)=>{
-            createUniqueWith(model,field,key);
-            createCompositeKeys(model,field,key);
+    return {
 
-        });
-    });
-        
+        initialize(done) {
 
-      sails.log.info("Hook:Composite initialized");
-      return done();
+            var models = sails.models;
+            _.each(models, model => {
+                _.each(model.attributes, (field, key) => {
+                    createUniqueWith(model, field, key);
+                    createCompositeKeys(model, field, key);
+
+                });
+            });
+
+
+            sails.log.info("Hook:Composite initialized");
+            return done();
+        }
+
+
     }
-
-    
-  }
 }
 
-function createCompositeKeys (model,field,key) {
-    if(_.isObject(field.composite) && _.isArray(field.composite.compositeOn)){
-        
-        model.beforeValidate=beforeValidate(key,field.composite.compositeOn,model['beforeValidate']);
+function createCompositeKeys(model, field, key) {
+    if (_.isObject(field.composite) && _.isArray(field.composite.compositeOn)) {
+
+        model.beforeValidate = beforeValidate(key, field.composite.compositeOn, model['beforeValidate']);
         addCompositeType(model);
-        return ;        
+        return;
     }
 }
 
-function createUniqueWith(model,field,key){
-  //  sails.log.info(field);
-    var keys=[];
-    if(_.isObject(field.uniqueWith) && _.isArray(field.uniqueWith.fields)){
-        var composite={};
-        keys=[key].concat(field.uniqueWith.fields);
-        var fieldName=keys.join('_');
-        composite[fieldName]={type:'string',required:true,unique:true};
+function createUniqueWith(model, field, key) {
+    //  sails.log.info(field);
+    var keys = [];
+    if (_.isObject(field.uniqueWith) && _.isArray(field.uniqueWith.fields)) {
+        var composite = {};
+        keys = [key].concat(field.uniqueWith.fields);
+        var fieldName = keys.join('_');
+        composite[fieldName] = { type: 'string', required: true, unique: true };
 
-        _.defaults(model.attributes,composite);
+        _.defaults(model.attributes, composite);
 
-        model.beforeValidate=beforeValidate(fieldName,keys,model['beforeValidate']);
+        model.beforeValidate = beforeValidate(fieldName, keys, model['beforeValidate']);
+
+
+        if (field.uniqueWith.uppercase) {
+            sails.log.info('Has field uppercase');
+            model.afterValidate = afterValidate(fieldName, keys, model['afterValidate']);
+        }
+
         addUniqueWithType(model);
         return;
     }
-        
+
 }
 
 
-function beforeValidate(composite,fields,callback){
-   
-    var monkey= function(model,cb){
-        sails.log.info(composite);
-        var composites=[];
-        _.forEach( fields,function(field){ sails.log.info(model[field]);composites.push(model[field]);});
+function beforeValidate(fieldName, fields, callback) {
+
+    var monkey = function(model, cb) {
+        sails.log.info(fieldName);
+        var composites = [];
+        _.forEach(fields, function(field) {
+            sails.log.info(model[field]);
+            composites.push(model[field]);
+        });
         sails.log.info(composites);
-        model[composite] = composites.join(':');
-        sails.log.info(model[composite]);
-        if(_.isFunction(callback)){
-            callback(model,cb);
-        }else{
+        model[fieldName] = composites.join(':');
+        sails.log.info(model[fieldName]);
+        if (_.isFunction(callback)) {
+            callback(model, cb);
+        } else {
             cb();
         }
     }
     return monkey;
 
-    
+
 }
 
-function addCompositeType(model){
-    if(_.isUndefined(model.types))
-        model.types={};
-    if( !('composite' in model.types)){
-        model.types.composite=function(value){return true;};
+function afterValidate(fieldName, fields, callback) {
+    var monkey = function(model, cb) {
+        _.forEach(fields, function(field) {
+            sails.log.info(field);
+            model[field] = model[field].toUpperCase();
+        });
+        if (_.isFunction(callback)) {
+            callback(model, cb);
+        } else {
+            cb();
+        }
+    };
+    return monkey;
+}
+
+function addCompositeType(model) {
+    if (_.isUndefined(model.types))
+        model.types = {};
+    if (!('composite' in model.types)) {
+        model.types.composite = function(value) { return true; };
 
     }
 }
 
-function addUniqueWithType(model){
-    if(_.isUndefined(model.types))
-        model.types={};
-    if( !('uniqueWith' in model.types)){
-        model.types.uniqueWith=function(value){return true;};
+function addUniqueWithType(model) {
+    if (_.isUndefined(model.types))
+        model.types = {};
+    if (!('uniqueWith' in model.types)) {
+        model.types.uniqueWith = function(value) { return true; };
 
     }
 }
--- api/models/Article.js
+++ api/models/Article.js
@@ -4,30 +4,29 @@
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
-var _ = require('lodash');
+
 module.exports = {
 
-  attributes: {
-    /*societe_code:{
-      type:'string',
-      required:true,
-      unique:true,
+    attributes: {
 
-      composite:{
-        compositeOn:['societe','code']
-      }
-    },*/
-    societe:{
-      type:'string',
-      required:true,
-      uniqueWith:{
-        fields:['code']
-      }
+        societe: {
+            type: 'string',
+            required: true,
+            uniqueWith: {
+                fields: ['code', 'complementaire'],
+                uppercase: true
+            }
+        },
+        code: {
+            type: 'string',
+            required: true
+        },
+        complementaire: {
+            type: 'string',
+            required: true
+        },
+        libelle: {
+            type: 'string',
+        }
     },
-    code:{
-      type:'string',
-      required:true
-    }
-  },
 };
-
